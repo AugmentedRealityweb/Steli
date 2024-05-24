082f529bb0733dd37fe16c87fdec3596012f6195
AugmentedRealityweb <160763410+AugmentedRealityweb@users.noreply.github.com>
2024-05-24 03:50:56 +0300

    Update send-email.js

--- send-email.js
+++ send-email.js
@@ -1,28 +1,28 @@
 const nodemailer = require('nodemailer');
 
 export default async function handler(req, res) {
-  const { name, phone, email, reservationDate, eventType } = req.body;
+    const { name, phone, email, reservationDate, eventType } = req.body;
 
-  let transporter = nodemailer.createTransport({
-    service: 'yahoo',
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
+    let transporter = nodemailer.createTransport({
+        service: 'yahoo',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+
+    let mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: process.env.EMAIL_TO,
+        subject: 'Cerere Rezervare',
+        text: `Nume: ${name}\nNumăr Telefon: ${phone}\nEmail: ${email}\nData Rezervării: ${reservationDate}\nTipul Evenimentului: ${eventType}`
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        res.status(200).json({ message: 'Email trimis cu succes' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Eroare la trimiterea emailului' });
     }
-  });
-
-  let mailOptions = {
-    from: process.env.EMAIL_USER,
-    to: process.env.EMAIL_TO,
-    subject: 'Cerere Rezervare',
-    text: `Nume: ${name}\nNumăr Telefon: ${phone}\nEmail: ${email}\nData Rezervării: ${reservationDate}\nTipul Evenimentului: ${eventType}`
-  };
-
-  try {
-    await transporter.sendMail(mailOptions);
-    res.status(200).json({ message: 'Email trimis cu succes' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Eroare la trimiterea emailului' });
-  }
-} 
+}
